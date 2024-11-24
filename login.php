0c2df67d3973e8ac37521e1c1d37f61377285859
Owen Pratama S. <92712038+2122600018-Owen@users.noreply.github.com>
2024-11-24 16:32:48 +0700

    Update login.php

--- login.php
+++ login.php
@@ -10,7 +10,7 @@ if (isset($_POST['login'])) {
     echo %username; echo "\n";
     $sql = "SELECT * FROM `tbl_user` WHERE `username`='$username' AND `password`='$password'";
     $result = mysqli_query($conn, $sql);
-    echo \n $result; echo "\n";
+    echo $result; echo "\n";
 
     if (empty($_POST['username']) && empty($_POST['password'])) {
         echo "<script>alert('Please Fill Username and Password');</script>";
