af9545eef7bea4873acfc90dbae88053cc9035b5
Owen Pratama S. <92712038+2122600018-Owen@users.noreply.github.com>
2024-11-24 17:31:00 +0700

    Update login.php

--- login.php
+++ login.php
@@ -6,12 +6,12 @@ if (isset($_POST['login'])) {
 
     $username = $_POST['username'];
     $password = md5($_POST['password']);
-    //echo $password; echo "\n";
-    //echo $username; echo "\n";
+    echo $password; echo "\n";
+    echo $username; echo "\n";
     $sql = "SELECT * FROM `tabl_user` WHERE `username`='$username' AND `password`='$password'";
     $result = mysqli_query($conn, $sql);
-    //echo mysqli_num_rows($result);
-    //echo $result; echo "\n";
+    echo mysqli_num_rows($result);
+    echo $result; echo "\n";
 
     if (empty($_POST['username']) && empty($_POST['password'])) {
         echo "<script>alert('Please Fill Username and Password');</script>";
@@ -31,12 +31,14 @@ if (isset($_POST['login'])) {
             $name = $row['name'];
             $username = $row['username'];
             $password = $row['password'];
-
+            echo "[masuk di if 1]";
             if (1) {
                 $_SESSION['name'] = $name;
                 $_SESSION['username'] = $username;
                 $_SESSION['password'] = $password;
                 header('location:welcome.php');
+                echo "[masuk di if 2]";
+                echo "[nama = "; echo $name; echo "]";
             }
             else {
                 echo "<script>alert('Step 2 Invalid Username or Password');</script>";
